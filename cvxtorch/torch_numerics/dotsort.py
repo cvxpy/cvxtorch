d7a3d658aa7c93dc9502dcb0e0559a758b43052b
Amit Solomon <as3993@princeton.edu>
2024-09-05 09:28:18 +0300

    Fixed dotsort.py.

--- cvxtorch/torch_numerics/dotsort.py
+++ cvxtorch/torch_numerics/dotsort.py
@@ -1,7 +1,16 @@
 import torch
 from cvxpy.expressions.expression import Expression
+import numpy as np
 
+
+def _get_args_from_values(values: list[np.ndarray]) -> tuple[torch.Tensor, torch.Tensor]:
+    x = values[0].flatten()
+    w = values[1].flatten()
+
+    w_padded = torch.zeros_like(x)  # pad in case size(W) < size(X)
+    w_padded[:len(w)] = w
+    return x, w_padded
 
 def torch_numeric(expr: Expression, values: list[torch.Tensor]) -> torch.Tensor:
-    x, w_padded = expr._get_args_from_values(values, mod=torch)
+    x, w_padded = _get_args_from_values(values)
     return torch.sort(x)[0] @ torch.sort(w_padded)[0]

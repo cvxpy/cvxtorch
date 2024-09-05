70fc7c2bb3589d939616d8240cf5ded85a9200e3
Amit Solomon <as3993@princeton.edu>
2024-09-05 08:42:26 +0300

    Corrected max.py.

--- cvxtorch/torch_numerics/max.py
+++ cvxtorch/torch_numerics/max.py
@@ -3,10 +3,5 @@ from cvxpy.expressions.expression import Expression
 
 
 def torch_numeric(expr: Expression, values: list[torch.Tensor]) -> torch.Tensor:
-    X = values[0]
-    P = values[1]
-    if expr.args[0].is_complex():
-        product = (torch.conj(X)).T @ (torch.linalg.inv(P)) @ X
-    else:
-        product = (X.T) @ (torch.linalg.inv(P)) @ X
-    return product.trace() if len(product.shape) == 2 else product
+    value = values[0]
+    return value.max()

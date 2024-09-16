4ea58cb54044e2cf54ad52f884067b17234c99b8
Amit Solomon <as3993@princeton.edu>
2024-09-16 12:44:10 -0400

    Changed upper_tri.py to support versions <3.11.

--- cvxtorch/torch_numerics/affine/upper_tri.py
+++ cvxtorch/torch_numerics/affine/upper_tri.py
@@ -1,7 +1,12 @@
 import torch
 from cvxpy.expressions.expression import Expression
 
-
-def torch_numeric(expr: Expression, values: list[torch.Tensor]) -> torch.Tensor:
+def torch_numeric(expr: Expression, values: list[torch.Tensor]) -> torch.Tensor:    
     inds = torch.triu_indices(row=values[0].shape[0], col=values[0].shape[1], offset=1)
-    return values[0][*inds]
+    # This can be simplified as `return values[0][*inds]`. However, this doesn't work on <3.11.
+    # I ended up doing the following solution to support both versions (checking the version
+    # during runtime causes issues during testing).
+    # In future Python versions >=3.11, this should be replaced.
+    if len(inds) != 2:
+        raise ValueError(f"Expected inds to be of length 2, but got {len(inds)} instead.")
+    return values[0][inds[0], inds[1]]

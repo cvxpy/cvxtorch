517d6d0ad329373e36bb6fdc0fb6a5f305d2c29a
Amit Solomon <as3993@princeton.edu>
2025-03-10 12:56:04 -0400

    Simplified one_minus_pos.

--- cvxtorch/torch_numerics/one_minus_pos.py
+++ cvxtorch/torch_numerics/one_minus_pos.py
@@ -3,6 +3,5 @@ from cvxpy.expressions.expression import Expression
 
 
 def torch_numeric(expr: Expression, values: list[torch.Tensor]) -> torch.Tensor:
-    # return torch.ones(values[0].shape) - values[0]
-    # 1+values[0].new(values[0].shape) creates a ones tensor in the same device as values[0]
-    return 1+values[0].new(values[0].shape)-values[0]
+    # return torch.ones(values[0].shape) - values[0]. Should be in the same device.
+    return 1-values[0]

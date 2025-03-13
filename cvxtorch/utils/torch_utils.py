4dc06c182af10b65cd779f5e22a64f1e3d24fb9b
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2025-03-13 12:33:45 -0400

    revert reshape change

--- cvxtorch/utils/torch_utils.py
+++ cvxtorch/utils/torch_utils.py
@@ -7,6 +7,7 @@ from scipy.sparse import coo_matrix, issparse
 
 VAR_TYPE = Enum("VAR_TYPE", "VARIABLE_PARAMETER CONSTANT EXPRESSION")
 
+
 def gen_tensor(value, dtype=torch.float64) -> torch.Tensor:
     """This function generates a tensor from an np.array or a sparse matrix.
     If the input is a sparse matrix, a sparse tensor is generated."""
@@ -19,13 +20,17 @@ def gen_tensor(value, dtype=torch.float64) -> torch.Tensor:
     v = torch.FloatTensor(vals)
     return torch.sparse.FloatTensor(i, v, torch.Size(value_coo.shape)).to(dtype)
 
+
 def tensor_reshape_fortran(value: torch.Tensor, shape: tuple) -> torch.Tensor:
     """This function reshapes a tensor in Fortran order (similar to numpy.reshape with order="F").
     This functionality is not included in Pytorch."""
-    # Use permute instead of .T to avoid the deprecation warning
-    ndim = value.ndim
-    perm = torch.arange(ndim - 1, -1, -1)
-    return torch.reshape(value.permute(*perm), shape[::-1]).permute(*perm)
+    # reverse_shape = list(shape)
+    # reverse_shape.reverse() #reverse a list in place
+    # return torch.reshape(value.reshape(reverse_shape).t(), shape=shape)
+    # A more compact solution based on
+    # https://stackoverflow.com/questions/64433896/pytorch-equivalent-of-numpy-reshape-function.
+    return torch.reshape(value.T, shape[::-1]).T
+
 
 def get_torch_numeric(expr: Expression) -> callable:
     """
@@ -35,6 +40,7 @@ def get_torch_numeric(expr: Expression) -> callable:
     Otherwise, get the default one provided by this pacjage (from EXPR2TORCH).
     """
     from cvxtorch.utils.exp2tch import EXPR2TORCH
+
     torch_numeric = getattr(expr, "torch_numeric", None)
     if torch_numeric:
         return torch_numeric

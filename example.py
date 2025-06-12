e9fe44e61b78160a42db70e190a2cf51b8115dbd
Amit Solomon <as3993@princeton.edu>
2025-06-12 12:10:15 -0400

    Edited the example

--- example.py
+++ example.py
@@ -11,6 +11,8 @@ exp = x-y+2*z
 tch_x = torch.arange(1, n+1)
 tch_y = torch.arange(0, n)
 
-tch_exp = TorchExpression(exp).torch_expression #tch_exp implements x-y+2*z, where x and y are torch.Tensor.
-tch_res = tch_exp(tch_x, tch_y) #Contains a torch.Tensor [7.0]*n
+tch_exp = TorchExpression(exp) #tch_exp implements x-y+2*z, where x and y are torch.Tensor.
+tch_exp_eval = tch_exp.torch_expression #Torch expression evaluation function.
+tch_exp_dict = tch_exp.variables_dictionary #Variables mapping
+tch_res = tch_exp_eval(tch_x, tch_y) #Contains a torch.Tensor [7.0]*n
 print(f"The result tensor is {tch_res}.")
